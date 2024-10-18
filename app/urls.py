d3c90afc51472f6b2f66b9d608ecdd3f8bd4e060
Usman <Cap.usman21@gmail.com>
2024-10-18 23:41:17 +0500

    Basic configurations and working with frontend configurations

---
+++ app/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+
+from app import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('post/<slug:slug>', views.post_page, name='post_page')
+]
