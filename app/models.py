b90e529b098e9cb50a030b7310d82126da2956a7
Usman <Cap.usman21@gmail.com>
2024-10-18 21:04:43 +0500

    Initial commit

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    title= models.CharField(max_length=200)
+    content= models.TextField()
+    last_updated= models.DateTimeField(auto_now=True)
+    slug= models.SlugField(max_length=200, unique=True)
+    images= models.ImageField( null=True, blank=True, upload_to='images/')
